2fdb39564df3281de89283d109ab409eda9803eb
Soreon <soreon@hotmail.fr>
2017-09-20 17:52:27 +0200

    Ajout des boutons Play/Pause et Reset Modification du design de la bestiole

--- js/Snake.js
+++ js/Snake.js
@@ -3,19 +3,47 @@
 
 var intervalSize = 5;
 
+function finFormula(x, t) {
+    return Math.abs(20*Math.log(x) - 1.5*x + 13 + Math.cos(0.4*(x-(t*0.04)))*10);
+}
+
+function bodyFormula(x) {
+    return 10 * Math.log(x) - 0.8 * (x-5);
+}
+
 function BodyPart(previous) {
     "use strict";
     this.previous = previous;
     this.head = previous.head;
     this.speed = previous.speed;
     this.order = this.previous.order + 1;
-    this.size = this.head.size * (1 - (this.order / this.head.numberOfParts));
+    //this.size = this.head.size * (1 - (this.order / this.head.numberOfParts));
+    this.size = bodyFormula(this.order);
     this.next = null;
     this.orientation = previous.orientation;
     this.position = {x: 0, y: 0};
 
     this.draw = function () {
         circle(this.position.x, this.position.y, this.size, "#0FF0FF");
+
+        // Ailerons
+        // var f = 0.25 * Math.pow(this.size, 2) - 2 * this.size + 13;
+
+        var base = this.size + 5;
+        var f = base + finFormula(this.order, Date.now());
+        var n = base;
+        var m = Math.toRadians(this.orientation - 110);
+        var p = Math.toRadians(this.orientation + 110);
+        var xmf = f * Math.cos(m);
+        var xmn = n * Math.cos(m);
+        var ymf = f * Math.sin(m);
+        var ymn = n * Math.sin(m);
+        var xpf = f * Math.cos(p);
+        var xpn = n * Math.cos(p);
+        var ypf = f * Math.sin(p);
+        var ypn = n * Math.sin(p);
+        line(this.position.x + xmn, this.position.y + ymn, this.position.x + xmf, this.position.y + ymf, "#FF0FF0");
+        line(this.position.x + xpn, this.position.y + ypn, this.position.x + xpf, this.position.y + ypf, "#FF0FF0");
 
         if (this.next) {
             this.next.draw();
@@ -66,7 +94,8 @@ function BodyPart(previous) {
     };
 
     this.updateSize = function() {
-        this.size = this.head.size * (1 - (this.order / this.head.numberOfParts));
+        //this.size = this.head.size * (1 - (this.order / this.head.numberOfParts));
+        this.size = bodyFormula(this.order);
     };
 
     this.setPosition();
@@ -81,17 +110,39 @@ function Snake(x, y) {
     this.order = 1;
     this.orientation = 0;
     this.speed = 5;
-    this.size = 20;
+    this.size = bodyFormula(1);
     this.numberOfParts = 1;
     this.wandering = false;
+    this.baseConeValue = 15;
+    this.instantConeValue = this.baseConeValue;
 
     this.draw = function () {
+        // Tête du snake
         circle(this.position.x, this.position.y, this.size, "#0FF0FF");
 
-        var radius = 30;
+        // Nez du snake
+        var radius = this.size + 10;
         var xp = radius * Math.cos(Math.toRadians(this.orientation));
         var yp = radius * Math.sin(Math.toRadians(this.orientation));
         circle(this.position.x + xp, this.position.y + yp, 2, "#0FF0FF");
+
+        // Ailerons
+        var base = this.size + 5;
+        var f = base + finFormula(this.order, Date.now());
+        var n = base;
+        var m = Math.toRadians(this.orientation - 110);
+        var p = Math.toRadians(this.orientation + 110);
+        var xmf = f * Math.cos(m);
+        var xmn = n * Math.cos(m);
+        var ymf = f * Math.sin(m);
+        var ymn = n * Math.sin(m);
+        var xpf = f * Math.cos(p);
+        var xpn = n * Math.cos(p);
+        var ypf = f * Math.sin(p);
+        var ypn = n * Math.sin(p);
+        line(this.position.x + xmn, this.position.y + ymn, this.position.x + xmf, this.position.y + ymf, "#FF0FF0");
+        line(this.position.x + xpn, this.position.y + ypn, this.position.x + xpf, this.position.y + ypf, "#FF0FF0");
+
         if (this.next) {
             this.next.draw();
         }
@@ -101,17 +152,16 @@ function Snake(x, y) {
 
         // Cas de figure ou le pointeur n'est pas sur le canvas, la bête se met à errer
         if (this.wandering) {
-            var randomConeValue = 15;
+            this.instantConeValue = this.baseConeValue;
             do {
                 var radius = 100;
-                var randomCone = (Math.random() * randomConeValue) - randomConeValue / 2;
+                var randomCone = (Math.random() * this.instantConeValue) - this.instantConeValue / 2;
                 var xp = radius * Math.cos(Math.toRadians(this.orientation + randomCone));
                 var yp = radius * Math.sin(Math.toRadians(this.orientation + randomCone));
                 
                 var resultX = this.position.x + xp;
                 var resultY = this.position.y + yp;
-                randomConeValue++;
-
+                this.instantConeValue++;
                 
             } while (resultX <= 0 || resultX >= window.innerWidth || resultY <= 0 || resultY >= window.innerHeight);
             
--- js/scripts.js
+++ js/scripts.js
@@ -32,6 +32,21 @@ function circle(x, y, radius, color) {
     context.stroke();
 }
 
+function line(ax, ay, bx, by, color, thickness) {
+    var funcStrokeStyle = context.strokeStyle;
+    var funcLineWidth = context.lineWidth;
+
+    context.beginPath();
+    context.moveTo(ax, ay);
+    context.lineTo(bx, by);
+    context.strokeStyle = color;
+    context.lineWidth = thickness;
+    context.stroke();
+
+    context.strokeStyle = funcStrokeStyle;
+    context.lineWidth = funcLineWidth;
+}
+
 function draw() {
     "use strict";
     clearCanvas();
@@ -51,6 +66,23 @@ function animate() {
         snake.move();
     }
     draw();
+}
+
+function togglePlay(play) {
+    start = play;
+    if(start) {
+        document.getElementById("play-button").blur();
+        document.getElementById("play-button").style.opacity = '0';
+        document.getElementById("play-button").style.zIndex = '0';  
+        document.getElementById("pause-button").style.opacity = '1';
+        document.getElementById("pause-button").style.zIndex = '1';
+    } else {
+        document.getElementById("pause-button").blur();
+        document.getElementById("pause-button").style.opacity = '0';
+        document.getElementById("pause-button").style.zIndex = '0'; 
+        document.getElementById("play-button").style.opacity = '1';
+        document.getElementById("play-button").style.zIndex = '1';
+    }
 }
 
 canvas.addEventListener("mousemove", function (event){
@@ -88,8 +120,8 @@ window.addEventListener("resize", function () {
 
 document.addEventListener("keypress", function (event) {
     "use strict";
-    if (event.keyCode === 13 && !start) {
-        start = true;
+    if (event.keyCode === 13) {
+        togglePlay(!start);
     }
     if (event.keyCode === 43) {
         snake.addBodyPart();
@@ -99,13 +131,20 @@ document.addEventListener("keypress", function (event) {
     }
 });
 
-document.getElementById("resetButton").addEventListener("click", function (){
+document.getElementById("reset-button").addEventListener("click", function (){
     "use strict";
     snake.position = {x: window.innerWidth / 2, y: window.innerHeight / 2};
     snake.orientation = 0;
-    start = false;
-    document.getElementById("resetButton").blur();
+    togglePlay(false);
+    document.getElementById("reset-button").blur();
 });
+
+document.getElementById("play-button").addEventListener("click", function() { togglePlay(true); });
+document.getElementById("pause-button").addEventListener("click", function() { togglePlay(false); });
+
+for(var i=0; i<64; i++) {
+    snake.addBodyPart();
+}
 
 animate();
 
