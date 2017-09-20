f7020409afc1a907059e6cd0a17f4d7faa017db2
Soreon <soreon@hotmail.fr>
2017-09-20 10:21:11 +0200

    Initialisation

---
+++ js/Snake.js
@@ -0,0 +1,152 @@
+/*jslint this */
+/*global circle */
+
+var intervalSize = 5;
+
+function BodyPart(previous) {
+    "use strict";
+    this.previous = previous;
+    this.head = previous.head;
+    this.speed = previous.speed;
+    this.order = this.previous.order + 1;
+    this.size = this.head.size * (1 - (this.order / this.head.numberOfParts));
+    this.next = null;
+    this.orientation = previous.orientation;
+    this.position = {x: 0, y: 0};
+
+    this.draw = function () {
+        circle(this.position.x, this.position.y, this.size, "#0FF0FF");
+
+        if (this.next) {
+            this.next.draw();
+        }
+    };
+
+    this.setPosition = function () {
+        var xp = intervalSize * Math.cos(Math.toRadians(360 + this.orientation));
+        var yp = intervalSize * Math.sin(Math.toRadians(360 + this.orientation));
+        this.position = {
+            x: previous.position.x - xp,
+            y: previous.position.y - yp
+        }
+    };
+
+    this.move = function () {
+        this.orientation = Math.toDegrees(Math.atan2(previous.position.y - this.position.y, previous.position.x - this.position.x));
+        var dist = Math.sqrt(Math.pow((previous.position.y - this.position.y), 2) + Math.pow((previous.position.x - this.position.x), 2));
+
+
+        var xp = (dist - intervalSize) * Math.cos(Math.toRadians(this.orientation));
+        var yp = (dist - intervalSize) * Math.sin(Math.toRadians(this.orientation));
+        this.position = {
+            x: this.position.x + xp,
+            y: this.position.y + yp
+        };
+        if (this.next) {
+            this.next.move();
+        }
+    };
+
+    this.addBodyPart = function () {
+        if (this.next) {
+            this.next.addBodyPart();
+        } else {
+            this.next = new BodyPart(this);
+        }
+        this.updateSize();
+    };
+
+    this.removeBodyPart = function () {
+        if (this.next.next) {
+            this.next.removeBodyPart();
+        } else {
+            this.next = null;
+        }
+        this.updateSize();
+    };
+
+    this.updateSize = function() {
+        this.size = this.head.size * (1 - (this.order / this.head.numberOfParts));
+    };
+
+    this.setPosition();
+}
+
+
+function Snake(x, y) {
+    "use strict";
+    this.position = {x: x, y: y};
+    this.next = null;
+    this.head = this;
+    this.order = 1;
+    this.orientation = 0;
+    this.speed = 5;
+    this.size = 20;
+    this.numberOfParts = 1;
+    this.wandering = false;
+
+    this.draw = function () {
+        circle(this.position.x, this.position.y, this.size, "#0FF0FF");
+
+        var radius = 30;
+        var xp = radius * Math.cos(Math.toRadians(this.orientation));
+        var yp = radius * Math.sin(Math.toRadians(this.orientation));
+        circle(this.position.x + xp, this.position.y + yp, 2, "#0FF0FF");
+        if (this.next) {
+            this.next.draw();
+        }
+    };
+
+    this.move = function () {
+
+        // Cas de figure ou le pointeur n'est pas sur le canvas, la bête se met à errer
+        if (this.wandering) {
+            var randomConeValue = 15;
+            do {
+                var radius = 100;
+                var randomCone = (Math.random() * randomConeValue) - randomConeValue / 2;
+                var xp = radius * Math.cos(Math.toRadians(this.orientation + randomCone));
+                var yp = radius * Math.sin(Math.toRadians(this.orientation + randomCone));
+                
+                var resultX = this.position.x + xp;
+                var resultY = this.position.y + yp;
+                randomConeValue++;
+
+                
+            } while (resultX <= 0 || resultX >= window.innerWidth || resultY <= 0 || resultY >= window.innerHeight);
+            
+            this.orientation = this.orientation + randomCone;
+        }
+
+        var xp = this.speed * Math.cos(Math.toRadians(this.orientation));
+        var yp = this.speed * Math.sin(Math.toRadians(this.orientation));
+        this.position = {
+            x: this.position.x + xp,
+            y: this.position.y + yp
+        };
+
+        if (this.next) {
+            this.next.move();
+        }
+    };
+
+    this.addBodyPart = function () {
+        this.numberOfParts++;
+        if (this.next) {
+            this.next.addBodyPart();
+        } else {
+            this.next = new BodyPart(this);
+        }
+    }
+
+    this.removeBodyPart = function () {
+        this.numberOfParts--;
+        if (this.next) {
+            if (this.next.next) {
+                this.next.removeBodyPart();
+            } else {
+                this.next = null;
+            }
+        }
+    };
+}
---
+++ js/scripts.js
@@ -0,0 +1,140 @@
+/*global document */
+/*global window */
+/*global Snake */
+
+var canvas = document.getElementById("main-canvas");
+var context = canvas.getContext("2d");
+var snake = new Snake(window.innerWidth / 2, window.innerHeight / 2);
+var start = false;
+var mousePosition = {x: 0, y: 0};
+
+(function () {
+    "use strict";
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    var x;
+    for (x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+
+    }
+}());
+
+Math.toRadians = function (degrees) {
+    "use strict";
+    return degrees * Math.PI / 180;
+};
+
+Math.toDegrees = function (radians) {
+    "use strict";
+    return radians * 180 / Math.PI;
+};
+
+function clearCanvas() {
+    "use strict";
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function circle(x, y, radius, color) {
+    "use strict";
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.lineWidth = 1;
+    context.strokeStyle = color;
+    context.stroke();
+}
+
+function draw() {
+    "use strict";
+    clearCanvas();
+    snake.draw();
+}
+
+function updateCanvasSize() {
+    "use strict";
+    canvas.width = canvas.scrollWidth;
+    canvas.height = canvas.scrollHeight;
+}
+
+function animate() {
+    "use strict";
+    if (start) {
+        window.requestAnimationFrame(animate);
+    }
+    snake.move();
+    draw();
+}
+
+canvas.addEventListener("mousemove", function (event){
+    "use strict";
+    mousePosition.x = event.clientX;
+    mousePosition.y = event.clientY;
+    snake.orientation = Math.toDegrees(Math.atan2(event.clientY - snake.position.y, event.clientX - snake.position.x));
+    draw();
+});
+
+canvas.addEventListener("mouseleave", function (event){
+    "use strict";
+    snake.wandering = true;
+
+    if(snake.position.x <= 0 || snake.position.x >= window.innerWidth || snake.position.y <= 0 || snake.position.y >= window.innerHeight) {
+        snake.position.x = window.innerWidth / 2;
+        snake.position.y = window.innerHeight / 2;
+    }
+});
+
+canvas.addEventListener("mouseenter", function (event){
+    "use strict";
+    snake.wandering = false;
+});
+
+window.addEventListener("load", function () {
+    "use strict";
+    updateCanvasSize();
+    draw();
+});
+
+window.addEventListener("resize", function () {
+    "use strict";
+    updateCanvasSize();
+    draw();
+});
+
+document.addEventListener("keypress", function (event) {
+    "use strict";
+    if (event.keyCode === 13 && !start) {
+        start = true;
+        animate();
+    }
+    if (event.keyCode === 43) {
+        snake.addBodyPart();
+        draw();
+    }
+    if (event.keyCode === 45) {
+        snake.removeBodyPart();
+        draw();
+    }
+});
+
+document.getElementById("resetButton").addEventListener("click", function (){
+    "use strict";
+    snake.position = {x: window.innerWidth / 2, y: window.innerHeight / 2};
+    snake.orientation = 0;
+    start = false;
+});
